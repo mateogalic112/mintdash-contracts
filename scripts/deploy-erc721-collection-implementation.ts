8e4c8794d2e1b8f208ebfb7ba6e9134660da5d29
mmarinovic <marko@blankhq.co>
2023-08-25 09:52:06 +0200

    add deploy script

---
+++ scripts/deploy-erc721-collection-implementation.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log(
+        "🔥 Deploying ERC721CollectionImplementation with the account: ",
+        deployer.address,
+    );
+
+    const ERC721CollectionImplementation = await ethers.getContractFactory(
+        "ERC721CollectionImplementation",
+    );
+    const implementation = await ERC721CollectionImplementation.deploy();
+    await implementation.deployed();
+    await implementation.initialize("Mintdash", "MINTDASH", deployer.address);
+
+    console.log(
+        "🚀 ERC721CollectionImplementation deployed to: ",
+        implementation.address,
+    );
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
